ada0a85dd90d3e1b0d0335e4a75ef9a3b45a5f55
NoShadiah <nabawangashadiah20@gmail.com>
2023-03-23 12:24:47 +0300

    documenting the api

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config# we’ll discuss the config file next
 from backend.db import db
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from flasgger import Swagger, swag_from
+from config.swagger import template, swagger_config
 
 
 
@@ -12,10 +15,17 @@ def create_app(config_name):
     config[config_name].init_app(app)
     app.config.JWT_SECRET_KEY="super secret key"
     app.config.from_pyfile("../config.py")
+    app.config.SWAGGER = {
+        "tittle ":"Online food delivery API",
+        "ui_version":3
+    }
 
 
     db.init_app(app)
+    # configurations with the app
     JWTManager(app)
+    CORS(app)
+    Swagger(app, config=swagger_config, template=template)
 
     from backend.users.controller import users
     # from backend.categories.controller import categories
